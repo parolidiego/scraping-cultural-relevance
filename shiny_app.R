ba47bc5c4d8f6c54b801d73029bfbc6b72a79c32
David Pereiro Pol <davidpereiropol@gmail.com>
2025-03-18 20:52:30 +0100

    Final Shiny app

--- shiny_app.R
+++ shiny_app.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(shinythemes)
 library(shinyjs)
 
+options(scipen = 999) # We remove the scientific notation
 
 ny <- read_csv("data/nyt_database.csv")
 movies <-  read_csv("data/box_office_database.csv")
@@ -16,85 +17,147 @@ data <-  movies |>
   full_join(ny, by = "date") |> 
   full_join(guardian, by = "date")
 
-data <- data %>%
-  mutate(across(everything(), ~ str_replace_all(.x, "<.+?>", "")))
+data <- data %>% # This is for cleaning the columns that where in HTML format
+  mutate(across(everything(), ~ str_replace_all(.x, "<.+?>", ""))) %>%
+  mutate(across(everything(), ~ if_else(.x == "NA", NA, .x)))
 
 
 
+# Interface
 
-# Interfaz de usuario mejorada
+
 ui <- fluidPage(
   useShinyjs(),
   theme = shinytheme("flatly"),
-  tags$head(
-    tags$style(HTML(".news-section { margin-bottom: 30px; } .bold-title { font-weight: bold; }"))
+  tags$head( # We create different styles for the titles
+    tags$style(HTML(".news-section { margin-bottom: 30px; } .bold-title { font-weight: bold; font-size: 24px; } .center-content { text-align: center; } .image-box { display: flex; justify-content: center; } .large-title { font-size: 28px; font-weight: bold; } .info-box { margin-top: 20px; padding: 10px; border: 1px solid #ddd; border-radius: 8px; background-color: #f9f9f9; } .extra-large-title { font-size: 32px; font-weight: bold; text-align: center; }"))
   ),
   titlePanel("Movie, Song and News of the Day"),
   sidebarLayout(
     sidebarPanel(
       dateInput("date", "Choose a date:", value = Sys.Date(), format = "yyyy-mm-dd", startview = "month", weekstart = 1)
     ),
+    # We set the structure of the page by creating different tabs
     mainPanel(
       tabsetPanel(
-        tabPanel("Overview",
-                 div(id = "content",
-                     fluidRow(
-                       column(6,
-                              h3("Most Successful Movie", class = "bold-title"),
-                              strong(textOutput("best_selling")),
-                              uiOutput("image")
-                       ),
-                       column(6,
-                              h3("Number 1 Song on Billboard", class = "bold-title"),
-                              strong(textOutput("song_artist")),
-                              uiOutput("image_url")
-                       )
-                     ),
-                     hr(),
-                     h3("Headlines of the Day", class = "bold-title"),
-                     div(class = "news-section",
-                         strong(uiOutput("news_link")),
-                         textOutput("subtitle")
-                     ),
-                     div(class = "news-section",
-                         strong(textOutput("headline")),
-                         textOutput("abstract")
-                     )
+        tabPanel("Movie",
+                 h3("Most Successful Movie", class = "bold-title center-content"),
+                 div(class = "image-box", uiOutput("image")),
+                 div(class = "large-title center-content", textOutput("best_selling")),
+                 div(class = "info-box",
+                     h4("Distributor", class = "bold-title"),
+                     textOutput("distributor"),
+                     h4("Genres", class = "bold-title"),
+                     textOutput("genres"),
+                     h4("Running Time", class = "bold-title"),
+                     textOutput("running_time"),
+                     h4("Daily Earnings", class = "bold-title"),
+                     textOutput("daily_earnings"),
+                     h4("Worldwide Earnings", class = "bold-title"),
+                     textOutput("world_wide_earnings"),
+                     h4("Description", class = "bold-title"),
+                     textOutput("description")
                  )
         ),
-        tabPanel("Movie Information",
-                 h3("Movie Earnings", class = "bold-title"),
-                 textOutput("daily_earnings"),
-                 h3("Movie Description", class = "bold-title"),
-                 textOutput("description")
+        tabPanel("Song",
+                 h3("Number 1 Song on Billboard", class = "bold-title center-content"),
+                 div(class = "image-box", uiOutput("image_url")),
+                 div(class = "large-title center-content", textOutput("song")),
+                 h4("Artist", class = "bold-title center-content"),
+                 div(class = "large-title center-content", textOutput("artist")),
+                 div(class = "info-box",
+                     h4("Weeks on Chart", class = "bold-title"),
+                     textOutput("weeks_on_chart"),
+                     h4("Weeks at #1", class = "bold-title"),
+                     textOutput("weeks_at_1"),
+                     h4("Last Week", class = "bold-title"),
+                     textOutput("last_week")
+                 )
+        ),
+        tabPanel("News",
+                 h2("Headlines of the Day", class = "extra-large-title"),
+                 h3("The Guardian", class = "bold-title center-content"),
+                 div(class = "news-section",
+                     div(class = "large-title", uiOutput("news_link")),
+                     textOutput("subtitle")
+                 ),
+                 h3("New York Times", class = "bold-title center-content"),
+                 div(class = "news-section",
+                     div(class = "large-title", textOutput("headline")),
+                     textOutput("lead_paragraph")
+                 )
         )
       )
     )
   )
 )
 
-# Lógica del servidor
+
+
+# Server logic
 server <- function(input, output, session) {
   
+  # We create a data frame that interacts with the date given
   filtered_data <- reactive({
     data %>% filter(date == input$date)
   })
   
+  # We extract the information that we need from the previous dataset
   output$best_selling <- renderText({
-    best_selling <- filtered_data()
-    if (nrow(best_selling) > 0) return(best_selling$best_selling) else return("There is no data for this day")
+    movie <- filtered_data()
+    if (nrow(movie) > 0) return(movie$best_selling) else return("No data available")
   })
   
   output$image <- renderUI({
-    best_selling <- filtered_data()
-    if (nrow(best_selling) > 0) {
-      img(src = best_selling$image, height = "300px", class = "img-responsive")
+    movie <- filtered_data()
+    if (nrow(movie) > 0) {
+      img(src = movie$image, height = "300px", class = "img-responsive")
     }
   })
   
-  output$song_artist <- renderText({
+  output$distributor <- renderText({
+    movie <- filtered_data()
+    if (nrow(movie) > 0) return(movie$distributor) else return("No data available")
+  })
+  
+  output$genres <- renderText({
+    movie <- filtered_data()
+    if (nrow(movie) > 0) return(movie$genres) else return("No data available")
+  })
+  
+  output$running_time <- renderText({
+    movie <- filtered_data()
+    if (nrow(movie) > 0) return(movie$running_time) else return("No data available")
+  })
+  
+  output$world_wide_earnings <- renderText({
+    movie <- filtered_data()
+    if (nrow(movie) > 0) return(paste("$", format(movie$worldwide_earnings, big.mark = ","))) else return("No data available")
+  })
+  
+  output$daily_earnings <- renderText({
+    movie <- filtered_data()
+    if (nrow(movie) > 0) return(paste("$", format(movie$daily_earnings, big.mark = ","))) else return("No data available")
+  })
+  
+  output$description <- renderText({
+    movie <- filtered_data()
+    if (nrow(movie) > 0) return(movie$description) else return("No data available")
+  })
+  
+  output$song <- renderText({
     song <- filtered_data()
-    if (nrow(song) > 0) return(paste(song$song, "-", song$artist)) else return("There is no data for this day")
+    if (nrow(song) > 0) return(song$song) else return("No data available")
+  })
+  
+  output$artist <- renderText({
+    song <- filtered_data()
+    if (nrow(song) > 0) return(song$artist) else return("No data available")
+  })
+  
+  output$last_week <- renderText({
+    song <- filtered_data()
+    if (nrow(song) > 0) return(song$last_week) else return("No data available")
   })
   
   output$image_url <- renderUI({
@@ -104,43 +167,40 @@ server <- function(input, output, session) {
     }
   })
   
+  output$weeks_on_chart <- renderText({
+    song <- filtered_data()
+    if (nrow(song) > 0) return(song$weeks_on_chart) else return("No data available")
+  })
+  
+  output$weeks_at_1 <- renderText({
+    song <- filtered_data()
+    if (nrow(song) > 0) return(song$weeks_at_1) else return("No data available")
+  })
+  
+  # We add an attribute to this headline to include the url of the newsspaper
   output$news_link <- renderUI({
-    news <- filtered_data()
-    if (nrow(news) > 0) {
-      tags$a(href = news$url, target = "_blank", news$title)
-    }
+    headlines <- filtered_data()
+    if (nrow(headlines) > 0) { if (is.na(headlines$title)) return("No data availabe") else
+      tags$a(href = headlines$url, target = "_blank", class = "large-title", headlines$title)
+    } else return("No data available")
   })
   
   output$subtitle <- renderText({
     headlines <- filtered_data()
-    if (nrow(headlines) > 0) return(headlines$subtitle) else return("")
+    if (nrow(headlines) > 0) if (is.na(headlines$subtitle)) return("") else return(headlines$subtitle)
   })
   
   output$headline <- renderText({
     headlines <- filtered_data()
-    if (nrow(headlines) > 0) return(headlines$headline) else return("")
+    if (nrow(headlines) > 0){if (is.na(headlines$headline)) return("No data available") else return(headlines$headline)} else return("No data available")
   })
   
-  output$abstract <- renderText({
+  output$lead_paragraph <- renderText({
     headlines <- filtered_data()
-    if (nrow(headlines) > 0) return(headlines$abstract) else return("")
+    if (nrow(headlines) > 0) if (is.na(headlines$lead_paragraph)) return("") else return(headlines$lead_paragraph)
   })
   
-  output$daily_earnings <- renderText({
-    movie <- filtered_data()
-    if (nrow(movie) > 0) return(paste("$", format(movie$daily_earnings, big.mark = ","))) else return("No earnings data available")
-  })
-  
-  output$description <- renderText({
-    movie <- filtered_data()
-    if (nrow(movie) > 0) return(movie$description) else return("No description available")
-  })
-  
-  observeEvent(input$date, {
-    runjs("document.getElementById('content').scrollIntoView({ behavior: 'smooth' });")
-  })
 }
 
-# Execut application
-
+# We execute application
 shinyApp(ui = ui, server = server)
